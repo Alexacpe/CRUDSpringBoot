175cb3400dc28031b79c5d18453f7b78d9212bc1
Alex Acevedo <alex.acpe@outlook.com>
2022-05-30 17:37:48 -0500

    compartir informacion desde el controlador a la vista

--- src/main/java/com/example/HolaSpring/CotroladorInicio.java
+++ src/main/java/com/example/HolaSpring/CotroladorInicio.java
@@ -1,7 +1,9 @@
 package com.example.HolaSpring;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 //El controlador es quien envia información al navegador
@@ -11,11 +13,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j//para enviar mensajes al log
 public class CotroladorInicio {
+
+    //acceder al valor que se da desde application properties
+    @Value("${index.saludo}") //se utiliza la misma llave valor que hay en application properties
+    private String saludo;
+
+
     //especificar la ruta en la que se va a ejecutar el controlador
     @GetMapping("/")
-    public String inicio (){
-
+    //compartir informacion con la vista
+    public String inicio (Model model){
+        var mensaje = " Mensaje con thymeleaf";
         log.info("ejecutando el controlador Spring MVC");
+        //se comarte la variable mensaje con la clase de modelo  con los atributos de llave valor
+        model.addAttribute("mensaje",mensaje);
+        //compartir la informacion tomada de applicatio properties
+        model.addAttribute("saludo",saludo);
         //devuelve un archivo html que se crea en resources
         return "index";
     };
