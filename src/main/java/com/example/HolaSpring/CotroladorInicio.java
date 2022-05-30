929d2e85948c142b015c54a9715066439ca1ec64
Alex Acevedo <alex.acpe@outlook.com>
2022-05-30 17:02:02 -0500

    configurando thymeleaft y mvc

--- src/main/java/com/example/HolaSpring/CotroladorInicio.java
+++ src/main/java/com/example/HolaSpring/CotroladorInicio.java
@@ -1,19 +1,22 @@
 package com.example.HolaSpring;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 //El controlador es quien envia información al navegador
 //restcontroller es para que spring indentifique esta clase como controlador
-@RestController
+
+@Controller
+
 @Slf4j//para enviar mensajes al log
 public class CotroladorInicio {
     //especificar la ruta en la que se va a ejecutar el controlador
     @GetMapping("/")
     public String inicio (){
 
-        log.info("ejecutando el controlador rest");
-        return "Hola mundo con Spring 2";
+        log.info("ejecutando el controlador Spring MVC");
+        //devuelve un archivo html que se crea en resources
+        return "index";
     };
 }
